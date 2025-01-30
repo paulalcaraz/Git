101d0675f63893a9a7fc9b7211e0573e52af6109
paulalcaraz <alcarazpaul4@gmail.com>
2025-01-30 20:59:14 +0800

    Console.WriteLine modified

--- Gii/Program.cs
+++ Gii/Program.cs
@@ -3,18 +3,57 @@
 
 
 
+//using System;
+
+//namespace MyFirstProgram
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//        Console.WriteLine("My Name is Paul");
+//        Console.Write("Hey");
+//        Console.WriteLine("I'm 18 years old");
+//        Console.Beep();
+
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
+
+
 using System;
 
 namespace MyFirstProgram
 {
     class Program
+
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("My Name is Paul");
-        Console.Write("Hey");
-        Console.WriteLine("I'm 18 years old");
-        Console.Beep();
+            int x;
+            x = 20;
+            int num;
+
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (x > num)
+            {
+                Console.WriteLine("The number higher");
+            } else if ( x < num)
+            {
+                Console.WriteLine("The nunber is lower");
+               
+            }
+            else {
+                Console.WriteLine("You got the corrrect answer!");
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
