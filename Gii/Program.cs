c078c42e390e91b3b66d7e386a4daa2f2d97c1a3
paulalcaraz <alcarazpaul4@gmail.com>
2025-01-30 20:14:06 +0800

    Console.WriteLine modified

--- Gii/Program.cs
+++ Gii/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, I'm Paul, 18 years old");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, I'm Paul, 18 years old");
+
+
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("My Name is Paul");
+        Console.WriteLine("I'm 18 years old");
+        Console.Beep();
+        }
+    }
+}
